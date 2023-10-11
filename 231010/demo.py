55c898aebec4819e011f1a8b2fde313e16605a7d
sunday <fj2379@gmail.com>
2023-10-11 12:12:18 +0900

    test

---
+++ 231010/demo.py
@@ -0,0 +1,25 @@
+import boto3
+
+resource = boto3.resource("dynamodb", region_name='ap-northeast-2', aws_access_key_id='AKIA5U4WVG6HFLMJUMOE', aws_secret_access_key='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')
+
+print(resource)
+client = boto3.client('dynamodb')
+response = client.describe_table(
+    TableName='lab_movie'
+)
+print(response)
+
+table = resource.Table('lab_movie')
+item = {
+    'Code' : '19780080',
+    'Name' : 'Star Wars',
+    'Genre' : 'SF',
+    'Date' : '1978-04-12',
+    'Director' : 'George Lucas',
+    'Actor' : '마크 해밀, 캐리 피셔, 해리슨 포드, 알렉 기네'
+}
+table.put_item(Item=item)
+
+
+
+
